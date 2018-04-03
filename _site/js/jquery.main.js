13a3c295cb4eb98152230e8e2af44c11a961849c
danbender <hello@danbender.net>
2018-04-03 18:54:05 +0200

    remove hover on touch devices

--- _site/js/jquery.main.js
+++ _site/js/jquery.main.js
@@ -16,6 +16,29 @@ function initFixedHeader(){
 	}
 }
 
+function hasTouch() {
+  return 'ontouchstart' in document.documentElement
+    || navigator.maxTouchPoints > 0
+    || navigator.msMaxTouchPoints > 0;
+}
+
+if (hasTouch()) { // remove all :hover stylesheets
+  try { // prevent exception on browsers not supporting DOM styleSheets properly
+    for (var si in document.styleSheets) {
+      var styleSheet = document.styleSheets[si];
+      if (!styleSheet.rules) continue;
+
+      for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
+        if (!styleSheet.rules[ri].selectorText) continue;
+
+        if (styleSheet.rules[ri].selectorText.match(':hover')) {
+          styleSheet.deleteRule(ri);
+        }
+      }
+    }
+  } catch (ex) {}
+}
+
 // mobile menu init
 function initMobileNav() {
 	jQuery('body').mobileNav({
@@ -691,4 +714,5 @@ jQuery(function(){
 	initSameHeight();
 	initFixedHeader();
 	initAnchors();
+  hasTouch();
 });
