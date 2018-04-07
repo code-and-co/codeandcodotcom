2d1046577c4bf24b521eb77913fed2672744b9f7
danbender <hello@danbender.net>
2018-04-07 22:00:30 +0200

    safari svg

--- _site/js/jquery.main.js
+++ _site/js/jquery.main.js
@@ -6,7 +6,7 @@ function isMobile() {
 
 function identifySafari() {
   if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) {
-    document.body.className += 'safari';
+    document.body.addClass('safari');
   };
 }
 
